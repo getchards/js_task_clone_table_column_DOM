c2090858b164ced416f882ce12365cef76fbd0cc
getchards <kazansasha02@gmail.com>
2024-06-08 14:01:33 +0300

    make build

---
+++ index.df68dedd.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){for(var e=document.getElementById("table").rows,n=0;n<e.length;n++){var t=e[n].cells,o=t[1].cloneNode(!0);e[n].insertBefore(o,t[t.length-1])}});
+//# sourceMappingURL=index.df68dedd.js.map
